3f085876535dec021bcae7f28bc005fe135141e2
mvmendoncas <88008850+mvmendoncas@users.noreply.github.com>
2023-02-03 17:22:35 -0300

    made onClick

--- app/src/main/java/com/example/layout/MainActivity.kt
+++ app/src/main/java/com/example/layout/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.layout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.layout.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCalculous.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.button_calculous){
+            calculate()
+        }
+    }
+
+    private fun calculate(){
+
+        val distance = binding.editDistance.text.toString().toFloat()
+        val price = binding.editPrice.text.toString().toFloat()
+        val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+        val totalValue = (distance * price) / autonomy
+        binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+       // Toast.makeText(this, totalValue.toString(), Toast.LENGTH_SHORT).show()
+
     }
 }
